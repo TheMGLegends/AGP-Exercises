12730efa47eb818f1b5ee1a5cba588513dd1e13e
TheMGLegends <matejovo34@gmail.com>
2024-10-24 12:32:45 +0100

    Week 7A Done

---
+++ AGP-Tutorial-07A/Source/Constant Buffers/CBufferWVP.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <DirectXMath.h>
+
+struct CBUFFER0
+{
+	DirectX::XMMATRIX WVP;
+};
---
+++ AGP-Tutorial-07A/Source/Window/Window.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <Windows.h>
+#include <memory>
+
+#include "../Renderer/Renderer.h"
+
+class Window
+{
+public:
+	Window();
+	HRESULT Init(HINSTANCE _hInstance, int nCmdShow, int width = 800, int height = 600);
+
+	static LRESULT CALLBACK HandleMessageSetup(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	static LRESULT CALLBACK HandleMessageRepeated(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	LRESULT HandleMessage(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	void OpenConsole();
+
+	void Run();
+
+	inline HWND GetWindowHandle() const { return hWnd; }
+	inline Renderer& GetRenderer() const { return *renderer; }
+
+private:
+	HINSTANCE hInstance;
+	HWND hWnd;
+	const wchar_t* windowName;
+
+	std::unique_ptr<Renderer> renderer;
+};
+
